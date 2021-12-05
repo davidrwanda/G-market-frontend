3265647e12eb6d76f18e45cbb77a5437a576e75b
davidrwanda <daventamag1@gmail.com>
2021-12-05 01:33:59 -1000

    crete products details

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import HomeScreen from './screen/HomeScreen'
+import ProductScreen from './screen/ProductScreen'
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome tp our platfom</h1>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} exact />
+          </Routes>
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   )
 }
 
